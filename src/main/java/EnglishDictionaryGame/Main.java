3f6fc4813ca8da4c1803676757763315e0fe7146
KineBeo <22028185@vnu.edu.vn>
2023-09-22 23:40:21 +0700

    First UI commit

---
+++ src/main/java/EnglishDictionaryGame/Main.java
@@ -0,0 +1,24 @@
+package EnglishDictionaryGame;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class                                                                             Main extends Application {
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 810, 500);
+    stage.setTitle("Dictionary!");
+    stage.setResizable(false);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch();
+    System.out.println("Hello, World!");
+  }
+}
