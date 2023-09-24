0b28a5cc58904341f6c22ff5bc3ca4f86695c4d2
Le Hoang Minh <71586973+Lehoangminh-dab@users.noreply.github.com>
2023-09-24 12:26:46 +0700

    Completed Dictionary class

--- src/main/java/EnglishDictionaryGame/Data/Dictionary.java
+++ src/main/java/EnglishDictionaryGame/Data/Dictionary.java
@@ -1,18 +1,38 @@
 package EnglishDictionaryGame.Data;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import org.apache.commons.lang3.StringUtils;
 
 public class Dictionary {
   private ArrayList<Word> words = new ArrayList<Word>();
 
+  private static final Comparator<Word> wordComparator = new Comparator<Word>() {
+    @Override
+    public int compare(Word word1, Word word2) {
+      return word1.getWordTarget().compareTo(word2.getWordTarget());
+    }
+  };
+
+  public Word getWord(int index) {
+    return words.get(index);
+  }
+
   public void addWord(Word word) {
+    word.setWordTarget(StringUtils.capitalize(word.getWordTarget()));
     words.add(word);
   }
 
-  public void addWord(String wordTarget, String wordExplain) {
-    Word word = new Word(wordTarget, wordExplain);
-    words.add(word);
+  public void removeWord(int index) {
+    words.remove(index);
   }
-  public Word getWord(int index) {
-    return words.get(index);
+
+  public int getSize() {
+    return words.size();
+  }
+
+  public void sortAlphabetically() {
+    Collections.sort(words, wordComparator);
   }
 }
