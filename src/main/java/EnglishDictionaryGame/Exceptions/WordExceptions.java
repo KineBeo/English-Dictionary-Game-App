061c1d1a3e7c4c617e7d02e2e6364f523c1614b0
Le Hoang Minh <71586973+Lehoangminh-dab@users.noreply.github.com>
2023-09-24 16:09:53 +0700

    Adjusted some spacing in WordExceptions.java, git pull test successful

--- src/main/java/EnglishDictionaryGame/Exceptions/WordExceptions.java
+++ src/main/java/EnglishDictionaryGame/Exceptions/WordExceptions.java
@@ -1,34 +1,29 @@
 package EnglishDictionaryGame.Exceptions;
 
 public class WordExceptions extends Exception {
-
   public WordExceptions(String message) {
     super(message);
   }
 
   public static class WordTargetIsEmptyException extends WordExceptions {
-
     public WordTargetIsEmptyException(String message) {
       super(message);
     }
   }
 
   public static class WordExplainIsEmptyException extends WordExceptions {
-
     public WordExplainIsEmptyException(String message) {
       super(message);
     }
   }
 
   public static class WordTargetContainsNewlineException extends WordExceptions {
-
     public WordTargetContainsNewlineException(String message) {
       super(message);
     }
   }
 
   public static class WordExplainContainsNewlineException extends WordExceptions {
-
     public WordExplainContainsNewlineException(String message) {
       super(message);
     }
