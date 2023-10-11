e9add86caf2d6bee27a6a2654cb88b2f51e4cd68
Le Hoang Minh <71586973+Lehoangminh-dab@users.noreply.github.com>
2023-10-11 14:52:29 +0700

    TextToSpeech debug commit 1

--- src/main/java/EnglishDictionaryGame/Main.java
+++ src/main/java/EnglishDictionaryGame/Main.java
@@ -27,6 +27,7 @@ public class Main extends Application {
   }
 
   public static void main(String[] args) {
+    TextToSpeech.playPronunciationSound("hello is it me you're looking for", TextToSpeech.LANGUAGE_ENGLISH);
     launch();
   }
 }
