3fdc2c4588e5ffd181baae89eb71a00038a43570
KineBeo <22028185@vnu.edu.vn>
2023-11-01 17:16:55 +0700

    Enable the application to be moved by dragging with the mouse.

--- src/main/java/EnglishDictionaryGame/Main.java
+++ src/main/java/EnglishDictionaryGame/Main.java
@@ -3,24 +3,39 @@ package EnglishDictionaryGame;
 import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
   public Stage mainStage;
+  double x, y;
 
   @Override
   public void start(Stage stage) {
     try {
       mainStage = stage;
       FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/Application.fxml"));
-      Scene scene = new Scene(fxmlLoader.load());
+      //      Scene scene = new Scene(fxmlLoader.load());
+      Parent root = fxmlLoader.load();
+      Scene scene = new Scene(root);
       scene
           .getStylesheets()
           .add(
               Objects.requireNonNull(Main.class.getResource("css/blueTheme.css")).toExternalForm());
       scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
+      root.setOnMousePressed(
+          mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+          });
+
+      root.setOnMouseDragged(
+          mouseEvent -> {
+            mainStage.setX(mouseEvent.getScreenX() - x);
+            mainStage.setY(mouseEvent.getScreenY() - y);
+          });
       mainStage.setResizable(false);
       Image icon =
           new Image(Objects.requireNonNull(Main.class.getResourceAsStream("image/icon.png")));
