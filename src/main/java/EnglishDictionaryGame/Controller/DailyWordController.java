05274fdf0ab749cab49a1e3a1fbd6c53af64edf5
KineBeo <22028185@vnu.edu.vn>
2023-11-01 19:21:27 +0700

    Daily word 95% finished.

--- src/main/java/EnglishDictionaryGame/Controller/DailyWordController.java
+++ src/main/java/EnglishDictionaryGame/Controller/DailyWordController.java
@@ -1,6 +1,7 @@
 package EnglishDictionaryGame.Controller;
 
 import EnglishDictionaryGame.Server.Trie;
+import java.time.LocalDate;
 import java.util.Random;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -14,6 +15,11 @@ public class DailyWordController extends WordOperation {
   @FXML private WebView webView;
   @FXML private Label randomWord;
 
+  private static LocalDate currentDate = LocalDate.now();
+  private static LocalDate previousDate;
+  private static String currentRandomWord;
+  private static String currentDefinition;
+
   @Override
   public void saveWord() {}
 
@@ -25,17 +31,48 @@ public class DailyWordController extends WordOperation {
 
   @FXML
   public void initialize() {
-    Random random = new Random();
-    String randomWord = Trie.getAllWordsFromTrie().get(random.nextInt(200));
-    this.randomWord.setText(randomWord);
-    String definition = Application.database.lookUpWord(randomWord);
-    definition =
-        "<html><body bgcolor='white' style='color:"
-            + "black"
-            + "; font-weight: bold; font-size: 20px;'>"
-            + definition
-            + "</body></html>";
+    //    if (!currentDate.equals(previousDate)) {
+    //      Random random = new Random();
+    //      String randomWord = Trie.getAllWordsFromTrie().get(random.nextInt(200));
+    //      this.randomWord.setText(randomWord);
+    //      String definition = Application.database.lookUpWord(randomWord);
+    //      definition =
+    //              "<html><body bgcolor='white' style='color:"
+    //                      + "black"
+    //                      + "; font-weight: bold; font-size: 20px;'>"
+    //                      + definition
+    //                      + "</body></html>";
+    //
+    //      webView.getEngine().loadContent(definition, "text/html");
+    //      previousDate = currentDate;
+    //    }
+    System.out.println(previousDate + " " + currentDate);
+    if (previousDate == null || !currentDate.equals(previousDate)) {
+      Random random = new Random();
+      currentRandomWord = Trie.getAllWordsFromTrie().get(random.nextInt(200));
+      this.randomWord.setText(currentRandomWord);
+      String definition = Application.database.lookUpWord(currentRandomWord);
+      definition =
+          "<html><body bgcolor='white' style='color:"
+              + "black"
+              + "; font-weight: bold; font-size: 20px;'>"
+              + definition
+              + "</body></html>";
 
-    webView.getEngine().loadContent(definition, "text/html");
+      webView.getEngine().loadContent(definition, "text/html");
+      previousDate = currentDate;
+      System.out.println(previousDate + " " + currentDate);
+    } else if (currentDate.equals(previousDate)) {
+        this.randomWord.setText(currentRandomWord);
+        String definition = Application.database.lookUpWord(currentRandomWord);
+        definition =
+            "<html><body bgcolor='white' style='color:"
+                + "black"
+                + "; font-weight: bold; font-size: 20px;'>"
+                + definition
+                + "</body></html>";
+
+        webView.getEngine().loadContent(definition, "text/html");
+    }
   }
 }
--- src/main/java/EnglishDictionaryGame/Main.java
+++ src/main/java/EnglishDictionaryGame/Main.java
@@ -1,5 +1,7 @@
 package EnglishDictionaryGame;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
