fe5607eec26992efb748e0732fb2767d09ba3ba1
KineBeo <22028185@vnu.edu.vn>
2023-11-22 00:09:16 +0700

    Hangman game debug.

--- src/main/java/EnglishDictionaryGame/Controller/WordsOfHangMan.java
+++ src/main/java/EnglishDictionaryGame/Controller/WordsOfHangMan.java
@@ -1,20 +1,25 @@
 package EnglishDictionaryGame.Controller;
 
 import EnglishDictionaryGame.Server.Trie;
+import EnglishDictionaryGame.Server.WordInfo;
+
 import java.util.ArrayList;
 import java.util.Random;
 
+import static EnglishDictionaryGame.Main.database;
+
 public class WordsOfHangMan {
-  private final ArrayList<String> words;
+  private final ArrayList<WordInfo> words;
 
   public WordsOfHangMan() {
     words = new ArrayList<>();
-    ArrayList<String> wordsFromDatabase = Trie.getAllWordsFromTrie();
+    ArrayList<WordInfo> wordsFromDatabase = database.getAllWordTargets();
     words.addAll(wordsFromDatabase);
     System.out.println(words.size());
   }
 
   public String getRandomWord() {
-    return words.get(new Random().nextInt(words.size())).toUpperCase();
+    int random = (int) (Math.random() * words.size());
+    return words.get(random).getWord().toUpperCase();
   }
 }
